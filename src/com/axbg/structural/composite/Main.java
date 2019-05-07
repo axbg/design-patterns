a518c24d8a52f74e882e0ee4d9e7360d7546c448
Alex Bisag <bisagalexstefan@gmail.com>
2019-05-07 23:46:57 +0300

    Init for Composite and Decorator Patterns.

---
+++ src/com/axbg/structural/composite/Main.java
@@ -0,0 +1,7 @@
+package com.axbg.structural.composite;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Composite pattern");
+    }
+}
---
+++ src/com/axbg/structural/decorator/Main.java
@@ -0,0 +1,7 @@
+package com.axbg.structural.decorator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Decorator pattern");
+    }
+}
